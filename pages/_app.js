f39bde2f05bda3e359a31347b8b4b5492d3474a6
Vince <37967982+Vinchenzo98@users.noreply.github.com>
2021-10-10 21:57:01 +0100

    front end update

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import { MoralisProvider } from 'react-moralis';
+import {ChakraProvider} from "@chakra-ui/react";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-    
+  return(
+
+    <MoralisProvider appId="XEu7NpoUqsIKwOFPI1xnm5RuwCckn8QFDusfIbaA" serverUrl="https://e2s7tswf4xf4.moralishost.com:2053/server">
+        <ChakraProvider>
+         <Component {...pageProps} />
+        </ChakraProvider> 
+    </MoralisProvider>
+  
+  )
 
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,168 @@
-import React from "react";
+import  React, {useState, useEffect} from 'react';
+import { useMoralis } from "react-moralis";
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {Button} from "@chakra-ui/button";
+import { Box} from "@chakra-ui/layout";
+import {Alert,AlertIcon,AlertTitle,AlertDescription, CloseButton } from "@chakra-ui/react"
 
-import { useMoralis } from "react-moralis";
 
-export default function App( ) {
-  
 
+
+export default function App() {
+  const { authenticate, isAuthenticated, logout, isAuthenticating, authError} = useMoralis();
+  const [web3, setWeb3] = useState(null)
+  const [address, setAddress] = useState(null)
+  const [contract, setContract] = useState(null)
+  const [totalSupply, setTotalSupply] = useState(0)
+
+  let abi = [{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"symbol","type":"string"},{"internalType":"string","name":"baseTokenURI","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"timestamp","type":"uint256"}],"name":"NumberMinted","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Paused","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"previousAdminRole","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"newAdminRole","type":"bytes32"}],"name":"RoleAdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"sender","type":"address"}],"name":"RoleGranted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"sender","type":"address"}],"name":"RoleRevoked","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Unpaused","type":"event"},{"inputs":[],"name":"DEFAULT_ADMIN_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"MINTER_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"OPERATOR_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleAdmin","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"getRoleMember","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleMemberCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"grantRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"hasRole","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"mintNumber","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"renounceRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"revokeRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"unpause","outputs":[],"stateMutability":"nonpayable","type":"function"},] // Paste your ABI here
+  let contractAddress = "0x26DF943610e5ced72e6D4EBe27B48F9e053cc744";
+
+  useEffect(() => {
+    window.ethereum ?
+      ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
+        setAddress(accounts[0])
+        let w3 = new Web3(ethereum)
+        setWeb3(w3)
+      
+        let c = new w3.eth.Contract(abi, contractAddress)
+        setContract(c)
+      
+        c.methods.totalSupply().call().then((_supply) => {
+          // Optionally set it to the state to render it using React
+          setTotalSupply(_supply)
+        }).catch((err) => console.log(err))
+      }).catch((err) => console.log(err))
+    : console.log("Please install MetaMask")
+  }, [])
+
+
+  function mint(){
+    let _price = web3.utils.toWei("1");
+    let encoded = contract.methods.safeMint().encodeABI()
+
+    let tx = {
+        from: address,
+        to : 0x26DF943610e5ced72e6D4EBe27B48F9e053cc744,
+        data : encoded,
+        nonce: "0x00",
+        value: web3.utils.numberToHex(_price)
+    }
+
+    let txHash = ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [tx],
+    }).then((hash) => {
+        alert("You can now view your transaction with hash: " + hash)
+    }).catch((err) => console.log(err))
+    
+    return txHash
+  }
+
+
+  if(!isAuthenticated){
     return (
       <div className={styles.container}>
-           <Head>
-             <title>Zero 2 Infinity</title>
-             <meta name="description" content="Generated by create next app" />
-             <link rel="icon" href="/favicon.ico" />
-           </Head>
-           <nav className={styles.nav}>
-             <div className={styles.navContainer}>
-             <ul className={styles.navBar}>
-                   <li className={styles.button} onClick={() => authenticate()}>Connect</li>          
-               </ul>
-             </div>
-            </nav> 
-     
-           <main className={styles.main}>
-          
-              
-              <h2 className={styles.title}>Zero to Infinity</h2>
-              <div className={styles.subContainer}>
-                <div className={styles.button1}>Mint</div>
-                <div className={styles.button2}>Whitepaper</div>
-              </div>
-           </main>
-           <div className={styles.img}></div>
-           <footer className={styles.footer}>
-             <a
-               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-               target="_blank"
-               rel="noopener noreferrer"
-             >
-               Powered by{' '}
-               <span className={styles.logo}>
-                 <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-               </span>
-             </a>
-           </footer>
-         </div> 
+        <div id="particles-js"></div>
+      <Head>
+        <title>Zero 2 Infinity</title>
+        <meta name="description" content="Generated by create next app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav className={styles.nav}>
+        <div className={styles.navContainer}>
+        <ul className={styles.navBar}>
+              <Button className={styles.button} onClick={() => authenticate()}>Connect</Button>         
+              <h2 className={styles.titleNav}>Zero to Infinity</h2> 
+          </ul>
+         
+        </div>
+       </nav> 
+  
+      <main className={styles.container2}>
+      {authError && (<Alert status="error">
+            <AlertIcon />
+            <Box flex="1">
+              <AlertTitle>Failed</AlertTitle>
+              <AlertDescription display="block">
+                {authError.message}
+              </AlertDescription>
+            </Box>
+            <CloseButton position="absolute" right="8px" top="8px" />
+          </Alert>)}
+         
+        
+         <div className={styles.subContainer}>
+  
+         </div>
+      </main>
+      <img className={styles.img} src="/backdrop_blk_.png" width={1920} height={1080}></img>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div> 
       
        )  
+  }
+    return(
+      <div className={styles.container}>
+        
+        <div id="particles-js"></div>
+      <Head>
+        <title>Zero 2 Infinity</title>
+        <meta name="description" content="Generated by create next app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav className={styles.nav}>
+        <div className={styles.navContainer}>
+        <ul className={styles.navBar}>
+        <h2 className={styles.titleNav}>Zero to Infinity</h2>
+              <Button className={styles.button} onClick={() => logout()} disabled={isAuthenticating}>Log Out</Button>          
+          </ul>
+        </div>
+       </nav> 
+       <img className={styles.img} src="/backdrop_blk_.png" width={1920} height={1080}></img>
+ 
+      <main className={styles.main}>
+        <div className={styles.subContainer}>
+        <Button className={styles.button1} onClick={() => mint()}>Mint</Button> 
+        <Button className={styles.button2}>Whitepaper</Button> 
+        </div>     
+      </main>
+     
+
+      
+      <footer className={styles.footer}>
+          <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+          </a>
+          </footer>
+    
+      </div>
+  
+      
+      
+    )
+  
   }
 
 
